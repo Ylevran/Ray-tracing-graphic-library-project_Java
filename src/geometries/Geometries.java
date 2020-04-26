02555abc869e42d92144711233bc2a4177771b98
Yossef Levran <yossef.levran@gmail.com>
2020-04-26 15:19:53 +0300

    recitation 3: files added

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,42 @@
+package geometries;
+
+import primitives.Point3D;
+import primitives.Ray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Geometries implements Intersectable {
+
+    private List<Intersectable> _geometries;
+
+    public Geometries(){
+        _geometries = new ArrayList<Intersectable>();
+    }
+
+    public Geometries(Intersectable... _geometries) {
+        add( _geometries);
+    }
+
+    public void add(Intersectable... geometries) {
+        for (Intersectable geo : geometries ) {
+            _geometries.add(geo);
+        }
+    }
+
+    @Override
+    public List<Point3D> findIntersections(Ray ray) {
+        List<Point3D> intersections = null;
+
+        for (Intersectable geo : _geometries) {
+            List<Point3D> tempIntersections = geo.findIntersections(ray);
+            if (tempIntersections != null) {
+                if (intersections == null)
+                    intersections = new ArrayList<Point3D>();
+                intersections.addAll(tempIntersections);
+            }
+        }
+        return intersections;
+    }
+
+}
