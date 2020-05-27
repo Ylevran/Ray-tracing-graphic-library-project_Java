cd174bfc043272928d363b5293865676cdadfe7f
shmuelse <44799431+shmuelse@users.noreply.github.com>
2020-05-27 13:18:42 +0300

    edit "AdvancedSpotLight static class" to "SpotLight class"

--- src/elements/SpotLight.java
+++ src/elements/SpotLight.java
@@ -12,7 +12,7 @@ import static primitives.Util.isZero;
  */
 public class SpotLight extends PointLight {
 
-    private Vector _direction;
+    protected Vector _direction;
     double _concentration;
 
     //***************** Constructors **********************//
@@ -48,18 +48,43 @@ public class SpotLight extends PointLight {
 
     @Override
     public Color getIntensity(Point3D p) {
-        double projection = _direction.dotProduct(getL(p));
 
-        if (isZero(projection)) {
-            return Color.BLACK;
-        }
-        double factor = Math.max(0, projection);
-        Color pointLightIntensity = super.getIntensity(p);
+        double dSquared = p.distanceSquared(_position);
+        double d = p.distance(_position);
 
-        if (_concentration != 1) {
-            factor = Math.pow(factor, _concentration);
-        }
+        Vector vector;
+        if(p.subtract(_position).normalized() == null)
+            vector = new Vector(_direction);
+        else
+            vector = p.subtract(_position).normalized();
 
-        return (pointLightIntensity.scale(factor));
+        return (_intensity.scale(Math.max(0,_direction.dotProduct(vector)))
+                .reduce(_kC + _kL * d + _kQ * dSquared));
+
     }
+
+    public static class AdvancedSpotLight extends SpotLight {
+
+
+        public AdvancedSpotLight(Color _colorIntensity, Point3D _position, Vector _direction, double _kC, double _kL, double _kQ, double _concentration) {
+            super(_colorIntensity, _position, _direction, _kC, _kL, _kQ ,_concentration);
+        }
+
+        @Override
+        public Color getIntensity(Point3D p) {
+            double dSquared = p.distanceSquared(_position);
+            double d = p.distance(_position);
+
+            Vector vector;
+            if(p.subtract(_position).normalized() == null)
+                vector = new Vector(_direction);
+            else
+                vector = p.subtract(_position).normalized();
+
+            return (_intensity.scale(Math.max(0,Math.pow(_direction.dotProduct(vector),_concentration)))
+                    .reduce(_kC + _kL * d + _kQ * dSquared));
+        }
+    }
+
+
 }
