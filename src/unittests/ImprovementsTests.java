01d8eecd1017f267d93253b0959d1a0458111e46
Yossef Levran <yossef.levran@gmail.com>
2020-06-17 20:49:40 +0300

    mini-project step 1 finished testing OK

--- src/unittests/ImprovementsTests.java
+++ src/unittests/ImprovementsTests.java
@@ -1,39 +1,30 @@
-/**
- *
- */
-package unittests.elements;
+package unittests;
 
-import geometries.Plane;
-import geometries.Polygon;
-import geometries.Tube;
 import org.junit.Test;
 
 import elements.*;
-import geometries.Sphere;
-import geometries.Triangle;
+import geometries.*;
 import primitives.*;
-import primitives.Color;
+import primitives.*;;
 import renderer.*;
 import scene.Scene;
 
-import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 
 /**
- * Tests for reflection and transparency functionality, test for partial shadows
- * (with transparency)
+ * Tests for improvements features:
+ * anti-aliasing
  *
- * @author dzilb
+ * @author Shmuel & Yossef
  *
  */
-public class ReflectionRefractionTests {
+public class ImprovementsTests {
 
     /**
      * Produce a picture of a sphere lighted by a spot light
      */
     @Test
-    public void twoSpheres() {
+    public void twoSpheresAdvanced() {
         Scene scene = new Scene("Test scene");
         scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
         scene.setDistance(1000);
@@ -48,20 +39,18 @@ public class ReflectionRefractionTests {
         scene.addLights(new SpotLight(new Color(1000, 600, 0), new Point3D(-100, 100, -500), new Vector(-1, 1, 2), 1,
                 0.0004, 0.0000006));
 
-        ImageWriter imageWriter = new ImageWriter("twoSpheres", 150, 150, 500, 500);
+        ImageWriter imageWriter = new ImageWriter("twoSpheres advanced", 150, 150, 500, 500);
         Render render = new Render(imageWriter, scene);
 
-        render.renderImage();
+        render.renderImageAdvanced();
         render.writeToImage();
     }
 
-
-
     /**
-     * Produce a picture of a sphere lighted by a spot light
+     * Produce an advance picture of a sphere lighted by a spot light
      */
     @Test
-    public void twoSpheresOnMirrors() {
+    public void twoSpheresOnMirrorsAdvanced() {
         Scene scene = new Scene("Test scene");
         scene.setCamera(new Camera(
                 new Point3D(0, 0, -10000),
@@ -96,44 +85,10 @@ public class ReflectionRefractionTests {
         scene.addLights(new SpotLight(new Color(1020, 400, 400), new Point3D(-750, 750, 150),
                 new Vector(-1, 1, 4), 1, 0.00001, 0.000005));
 
-        ImageWriter imageWriter = new ImageWriter("twoSpheresMirrored", 2500, 2500, 500, 500);
+        ImageWriter imageWriter = new ImageWriter("twoSpheresMirroredAdvanced", 2500, 2500, 500, 500);
         Render render = new Render(imageWriter, scene);
 
-        render.renderImage();
-        render.writeToImage();
-    }
-
-
-
-
-
-    /**
-     * Produce a picture of a two triangles lighted by a spot light with a partially transparent Sphere
-     *  producing partial shadow
-     */
-    @Test
-    public void trianglesTransparentSphere() {
-        Scene scene = new Scene("Test scene");
-        scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
-        scene.setDistance(1000);
-        scene.setBackground(Color.BLACK);
-        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
-
-        scene.addGeometries( //
-                new Triangle(Color.BLACK, new Material(0.5, 0.5, 60), //
-                        new Point3D(-150, 150, 115), new Point3D(150, 150, 135), new Point3D(75, -75, 150)), //
-                new Triangle(Color.BLACK, new Material(0.5, 0.5, 60), //
-                        new Point3D(-150, 150, 115), new Point3D(-70, -70, 140), new Point3D(75, -75, 150)), //
-                new Sphere(new Color(java.awt.Color.BLUE), new Material(0.2, 0.2, 30, 0.6, 0), // )
-                        30, new Point3D(60, -50, 50)));
-
-        scene.addLights(new SpotLight(new Color(700, 400, 400), //
-                new Point3D(60, -50, 0), new Vector(0, 0, 1), 1, 4E-5, 2E-7));
-
-        ImageWriter imageWriter = new ImageWriter("shadow with transparency", 200, 200, 600, 600);
-        Render render = new Render(imageWriter, scene);
-
-        render.renderImage();
+        render.renderImageAdvanced();
         render.writeToImage();
     }
 
@@ -141,7 +96,7 @@ public class ReflectionRefractionTests {
      * Produce a picture of a many geometries lighted by a directional light with refraction and reflection
      */
     @Test
-    public void coronaSphere() throws IOException {
+    public void coronaSphereAdvanced() throws IOException {
         Scene scene = new Scene("Test scene");
         scene.setCamera(new Camera(
                 new Point3D(0, 0, -200),
@@ -302,10 +257,10 @@ public class ReflectionRefractionTests {
 
         scene.addLights(new DirectionalLight(new Color(255, 191, 191), new Vector(1, -1, 1)));
 
-        ImageWriter imageWriter = new ImageWriter("coronaSphere", 200, 200, 500, 500);
+        ImageWriter imageWriter = new ImageWriter("coronaSphereAdvanced", 200, 200, 500, 500);
         Render render = new Render(imageWriter, scene);
 
-        render.renderImage();
+        render.renderImageAdvanced();
         render.writeToImage();
     }
 }
