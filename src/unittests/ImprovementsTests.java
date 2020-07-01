03f6dca87e96b295bf2a6f5f6473f925487e4835
Yossef Levran <yossef.levran@gmail.com>
2020-07-01 14:35:18 +0300

    finalTest high resolution

--- src/unittests/ImprovementsTests.java
+++ src/unittests/ImprovementsTests.java
@@ -269,7 +269,7 @@ public class ImprovementsTests {
         scene.addLights(new SpotLight(new Color(1000, 600, 0), new Point3D(-100, 100, -500), new Vector(-1, 1, 2), 1,
                 0.0004, 0.0000006, 30));
 
-        ImageWriter imageWriter = new ImageWriter("finalTest1", 150, 150, 500, 500);
+        ImageWriter imageWriter = new ImageWriter("finalTest1", 150, 150, 1000, 1000);
         Render render = new Render(imageWriter, scene);
 
         render.renderImage(false, false, false);
@@ -385,7 +385,7 @@ public class ImprovementsTests {
         scene.addLights(new SpotLight(new Color(1000, 600, 0), new Point3D(-100, 100, -500), new Vector(-1, 1, 2), 1,
                 0.0004, 0.0000006, 30));
 
-        ImageWriter imageWriter = new ImageWriter("finalTest2", 150, 150, 500, 500);
+        ImageWriter imageWriter = new ImageWriter("finalTest2", 150, 150, 1000, 1000);
         Render render = new Render(imageWriter, scene).
                 setMultithreading(3).
                 setDebugPrint();
